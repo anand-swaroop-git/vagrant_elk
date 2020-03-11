38a3c709a1983c797c855e019d1059c6ce9522ac
Anand Swaroop <25723629+anand-swaroop-git@users.noreply.github.com>
2020-03-11 15:44:55 +1030

    Update provision_kibana.sh

--- provision_kibana.sh
+++ provision_kibana.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 echo -e "\e[1;32mProvisioning now... \e[0m"
 echo
-echo
-echo
 echo -e "\e[1;32mDoing the GPG thing... \e[0m"
 sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
 echo -e "\e[1;32mAdding to the repo list... \e[0m"
-sudo echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list
+sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 
 echo -e "\e[1;32mUpdating apt and installing kibana... \e[0m"
 apt-get update && apt-get install kibana
@@ -17,7 +15,7 @@ echo -e "\e[1;32mEntering custom server.host, server.name and elasticsearch.url 
 sudo echo "
 server.host: \"10.0.15.12\"
 server.name: \"andy-kibana01\"
-elasticsearch.url: \"http://10.0.15.10:9200\"" >> /etc/kibana/kibana.yml
+elasticsearch.hosts: \"http://10.0.15.10:9200\"" >> /etc/kibana/kibana.yml
 echo
 echo
 #enable on bootup
@@ -27,6 +25,8 @@ sudo systemctl enable kibana
 
 echo -e "\e[1;32mStarting Kibana service now... \e[0m"
 sudo service kibana start
-echo
-echo
+
+echo -e "\e[1;32mChecking Kibana service status... \e[0m"
+sudo service kibana status
+
 echo -e "\e[1;32mThe provisioning of Kibana server has been done!\e[0m"
